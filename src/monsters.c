a63660cb66ac86e7aa1cf45e8bb07409a4850b92
Afonso Sousa <afonso@MBP-de-Afonso.Home>
2023-04-28 17:13:44 +0100

    criadas as funções que spawnam os 10 mobs, aleatoriamente no mapa, e que movem os monstros

--- src/monsters.c
+++ src/monsters.c
@@ -4,9 +4,73 @@
 #include <ncurses.h>
 #include "testeGeraMapa.h"
 
+typedef struct {
+    int x;
+    int y;
+    int health;
+    int attack;
+} Monster;
 
+Monster monsters[10];
 
-void set_monsterX(MONSTER *monster, int x){
+void spawn_mobs() {
+    // Inicializa o gerador de números aleatórios
+    srand(time(NULL));
+    
+    // Gera 10 monstros com posições aleatórias no mapa
+    for (int i = 0; i < 10; i++) {
+        int x, y;
+        do {
+            // Gera uma posição aleatória
+            x = rand() % MAP_WIDTH;
+            y = rand() % MAP_HEIGHT;
+        } while (mvinch(y, x) == '#'); // Verifica se a posição é uma parede
+        
+        // Inicializa o monstro com a posição gerada e atributos aleatórios
+        monsters[i].x = x;
+        monsters[i].y = y;
+        monsters[i].health = rand() % 10 + 1;
+        monsters[i].attack = rand() % 5 + 1;
+        
+        // Define o par de cor 1 como vermelho
+		init_pair(1, COLOR_RED, COLOR_BLACK);
+
+		// Ativa a cor vermelha para imprimir o monstro
+		attron(COLOR_PAIR(1));
+		mvaddch(monsters[i].monsterY, monster[i].monsterX, '!');
+		attroff(COLOR_PAIR(1));
+
+    }
+}
+
+void move_mobs() {
+    // Itera sobre a lista de monstros
+    for (int i = 0; i < 10; i++) {
+        int x, y;
+        do {
+            // Gera uma posição aleatória adjacente
+            x = monsters[i].x + rand() % 3 - 1;
+            y = monsters[i].y + rand() % 3 - 1;
+        } while (mvinch(y, x) == '#'); // Verifica se a posição é uma parede
+        
+        // Apaga o monstro da posição anterior
+        mvaddch(monsters[i].y, monsters[i].x, ' ');
+        
+        // Atualiza a posição do monstro
+        monsters[i].x = x;
+        monsters[i].y = y;
+        
+        // Define o par de cor 1 como vermelho
+		init_pair(1, COLOR_RED, COLOR_BLACK);
+
+		// Ativa a cor vermelha para imprimir o monstro
+		attron(COLOR_PAIR(1));
+		mvaddch(monsters[i].monsterY, monster[i].monsterX, '!');
+		attroff(COLOR_PAIR(1));
+    }
+}
+
+/*void set_monsterX(MONSTER *monster, int x){
     monster->monsterX = x;
 }
 
@@ -37,7 +101,7 @@ void inicializa_monster(MONSTER monstros[],int N){
 	}
 }
 
-/*
+
 void remove_monster(STATE *st){
 	int x = st->monster->monsterX;
 	int y = st->monster->monsterY;
